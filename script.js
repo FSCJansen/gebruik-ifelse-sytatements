0acfe76e04b4fecf3a7905d9d9a0d7e6f1b590fd
Frans Jansen <info@fransjansen.eu>
2021-09-08 15:40:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//Deel 1, Leeftijd check
+const age = 18;
+
+if (age > 17) {
+
+    console.log("Kom binnen, leuk dat je er bent. Maak er een gezellige avond van.");
+
+} else {
+
+    console.log("Je bent te jong om toegelaten te worden tot de kroeg. Ik verzoek je vriendelijk weg te gaan.");
+
+}
+
+//Deel 2, Ladiesnight
+const isFemale = true;
+
+if (isFemale) {
+
+    console.log("Je bent een dame en dus kun je een kaartje kopen voor de ladiesnight")
+
+} else {
+
+console.log("Helaas is de avond enkel voor vrouwen en daarom kom je niet in aanmerking voor een kaartje")
+
+}
+
+//Deel 3, Bob
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+
+    console.log("Je bent nuchter. Ik hoop dat je een leuke avond.")
+
+} else {
+
+    console.log("Ik bel een taxi voor je, want je mag niet meer zelf rijden.")
+
+}
